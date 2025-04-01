aac2f84016800a193bafe13c354e318419a61849
AparmitSrivastava <aparmitsrivastava1@gmail.com>
2025-04-01 20:21:08 +0530

    final

--- script.js
+++ script.js
@@ -4,6 +4,12 @@ console.log("start");
 let left = document.getElementById("leftbox")
 let right = document.getElementById("rightbox")
 let lists = document.getElementsByClassName("list")
+let cont = document.getElementById("container")
+
+cont.style.backgroundColor="#2235"
+document.body.style.padding=0
+document.body.style.margin=0
+document.body.style.maxHeight="100vh"
 
 let selected = null;
 
